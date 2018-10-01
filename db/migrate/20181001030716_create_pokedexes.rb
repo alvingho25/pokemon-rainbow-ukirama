e29c82eb12a447665ca6e807cbbcb40096f1a34c
Alvin <alvingho25@gmail.com>
2018-10-01 10:23:35 +0700

    Fix navbar link and add migration for pokedex

---
+++ db/migrate/20181001030716_create_pokedexes.rb
@@ -0,0 +1,16 @@
+class CreatePokedexes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pokedexes do |t|
+      t.string :name
+      t.integer :base_health_point
+      t.integer :base_attack
+      t.integer :base_defence
+      t.integer :base_speed
+      t.string :element_type
+      t.string :image_url
+
+      t.timestamps
+    end
+    add_index :pokedexes, :name, unique: true
+  end
+end
