0203750008505917d804c0ba253c6e03a1bb6a7b
Alvin <alvingho25@gmail.com>
2018-10-03 11:27:21 +0700

    add validation pokemon health to pokemon battle

--- app/models/pokemon_battle.rb
+++ app/models/pokemon_battle.rb
@@ -5,10 +5,23 @@ class PokemonBattle < ApplicationRecord
     belongs_to :loser, class_name: "Pokemon", foreign_key: "pokemon_loser_id", optional: true
 
     validate :same_pokemon
+    validate :pokemon_health
+
     private
     def same_pokemon
         if pokemon1_id == pokemon2_id
             errors.add(:base, " Pokemon2 can't be same with Pokemon1")
+        end
+    end
+
+    def pokemon_health
+        @pokemon1 = Pokemon.find(pokemon1_id)
+        @pokemon2 = Pokemon.find(pokemon2_id)
+        if @pokemon1.current_health_point == 0 
+            errors.add(:base, " Pokemon1 can't have 0 health to battle")
+        end
+        if @pokemon2.current_health_point == 0
+            errors.add(:base, " Pokemon2 can't have 0 health to battle")
         end
     end
 end
