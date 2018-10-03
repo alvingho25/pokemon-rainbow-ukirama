5c9d588dd234ad16fd6b87e2ee6f5653baf27a37
Alvin <alvingho25@gmail.com>
2018-10-03 11:06:02 +0700

    fix validation pokemon and layout view

--- app/models/pokemon_battle.rb
+++ app/models/pokemon_battle.rb
@@ -3,4 +3,12 @@ class PokemonBattle < ApplicationRecord
     belongs_to :pokemon2, class_name: "Pokemon", foreign_key: "pokemon2_id"
     belongs_to :winner, class_name: "Pokemon", foreign_key: "pokemon_winner_id", optional: true
     belongs_to :loser, class_name: "Pokemon", foreign_key: "pokemon_loser_id", optional: true
+
+    validate :same_pokemon
+    private
+    def same_pokemon
+        if pokemon1_id == pokemon2_id
+            errors.add(:base, " Pokemon2 can't be same with Pokemon1")
+        end
+    end
 end
