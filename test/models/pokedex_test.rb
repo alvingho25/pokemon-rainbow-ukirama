0537a1781fc13a9b46ba52caea120011cc61aac7
Alvin <alvingho25@gmail.com>
2018-10-05 10:17:35 +0700

    change test to more descriptive

--- test/models/pokedex_test.rb
+++ test/models/pokedex_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class PokedexTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   def setup
     @pokedex = Pokedex.new(
       name: "Alvin",
@@ -29,85 +26,106 @@ class PokedexTest < ActiveSupport::TestCase
     assert @pokedex.valid?, "#{@pokedex.element_type} should be valid element"
   end
 
-  test "name is not valid" do
+  test "pokedex name should not be null" do
     @pokedex.name = ""
     assert_not @pokedex.valid?, "Pokedex name should not be null"
-
-    @pokedex.name = "a" * 50
-    assert_not @pokedex.valid?, "Pokedex name length should not be greater than 45"
-
-    @pokedex_2 = @pokedex.dup
-    assert_not @pokedex_2.valid?, "Pokedex name should be unique"
   end
 
-  test "name is valid" do
+  test "pokedex name length should not be greater than 45" do
+    @pokedex.name = "a" * 50
+    assert_not @pokedex.valid?, "Pokedex name length should not be greater than 45"
+  end
+
+  test "pokedex name should not have same name" do
+    pokedex_2 = @pokedex.dup
+    @pokedex.save
+    assert_not pokedex_2.valid?, "Pokedex name should be unique"
+  end
+
+  test "pokedex name should be valid" do
     @pokedex.name = "Alvin"
     assert @pokedex.valid?, "Pokedex name should be valid"
+  end
 
+  test "pokedex name should be less or equal to 45" do
     @pokedex.name = "a" * 45
     assert @pokedex.valid?, "Pokedex name should be valid"
   end
 
-  test "base health point is not valid" do
+  test "pokedex name should be unique" do
+    pokedex_2 = @pokedex.dup
+    pokedex_2.name = "Duplicated"
+    assert pokedex_2.valid?, "Pokedex name should be valid and unique"
+  end
+
+  test "pokedex base health point should not be less or equal 0" do
     @pokedex.base_health_point = 0
-    assert_not @pokedex.valid?, "Pokedex base health point should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base health point should not be 0"
 
     @pokedex.base_health_point = -10
-    assert_not @pokedex.valid?, "Pokedex base health point should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base health point should not be less than 0"
+  end
 
+  test "pokedex base health point should not be string" do
     @pokedex.base_health_point = "a"
     assert_not @pokedex.valid?, "Pokedex base health point should not be string"
   end
 
-  test "base health point is valid" do
+  test "pokedex base health point should be valid" do
     @pokedex.base_health_point = "500"
     assert @pokedex.valid?, "Pokedex base health point should be valid"
   end
 
-  test "base attack is not valid" do
+  test "pokedex base attack should not be less or equal 0" do
     @pokedex.base_attack = 0
-    assert_not @pokedex.valid?, "Pokedex base attack should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base attack should not be 0"
 
     @pokedex.base_attack = -10
-    assert_not @pokedex.valid?, "Pokedex base attack should be greater than"
-    
+    assert_not @pokedex.valid?, "Pokedex base attack should not be less than 0"
+  end
+
+  test "pokedex base attack should not be string" do
     @pokedex.base_attack = "a"
     assert_not @pokedex.valid?, "Pokedex base attack should not be string"
   end
 
-  test "base attack is valid" do
+  test "pokedex base attack is valid" do
     @pokedex.base_attack = 500
     assert @pokedex.valid?, "Pokedex base attack should be valid"
   end
 
-  test "base defence is not valid" do
+  test "pokedex base defence should not be less or equal 0" do
     @pokedex.base_defence = 0
-    assert_not @pokedex.valid?, "Pokedex base defence should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base defence should not be 0"
 
     @pokedex.base_defence = -5
-    assert_not @pokedex.valid?, "Pokedex base defence should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base defence should not be less than 0"
+  end
 
+  test "pokedex base defence should not be string" do
     @pokedex.base_defence = "a"
     assert_not @pokedex.valid?, "Pokedex base defence should not be string"
   end
 
-  test "base defence is valid" do
+  test "pokedex base defence should be valid" do
     @pokedex.base_defence = 500
     assert @pokedex.valid?, "Pokedex base defence should be valid"
   end
 
-  test "base speed is not valid" do
+  test "pokedex base speed should not be less or equal 0" do
     @pokedex.base_speed = 0
-    assert_not @pokedex.valid?, "Pokedex base speed should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base speed should not be 0"
 
     @pokedex.base_speed = -5
-    assert_not @pokedex.valid?, "Pokedex base speed should be greater than 0"
+    assert_not @pokedex.valid?, "Pokedex base speed should not be less than 0"
+  end
 
+  test "pokedex base speed should not be string" do
     @pokedex.base_speed = "a"
     assert_not @pokedex.valid?, "Pokedex base speed should not be string"
   end
 
-  test "base speed is valid" do
+  test "pokedexbase speed should be valid" do
     @pokedex.base_speed = 500
     assert @pokedex.valid?, "Pokedex base speed should be valid"
   end
--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -23,55 +23,71 @@ class SkillTest < ActiveSupport::TestCase
     assert @skill.valid?, "#{@skill.element_type} should be valid element"
   end
 
-  test "name is not valid" do
+  test "skill name should not be null" do
     @skill.name = ""
     assert_not @skill.valid?, "skill name should not be null"
-
-    @skill.name = "a" * 50
-    assert_not @skill.valid?, "skill name length should not be greater than 45"
-
-    @skill_2 = @skill.dup
-    assert_not @skill_2.valid?, "skill name should be unique"
   end
 
-  test "name is valid" do
+  test "skill name should not be greater than 45" do
+    @skill.name = "a" * 50
+    assert_not @skill.valid?, "skill name length should not be greater than 45"
+  end
+
+  test "skill name should not have same name" do
+    skill_2 = @skill.dup
+    @skill.save
+    assert_not skill_2.valid?, "skill name should be unique"
+  end
+
+  test "skill name should be valid" do
     @skill.name = "Scorching Fire"
     assert @skill.valid?, "skill name should be valid"
+  end
 
+  test "skill name should be less or equal than 45" do
     @skill.name = "a" * 45
     assert @skill.valid?, "skill name should be valid"
   end
 
-  test "power is not valid" do
+  test "skill name should be unique" do
+    skill_2 = @skill.dup
+    skill_2.name = "Duplicated"
+    assert @skill.valid?, "skill name should valid and unique"
+  end
+
+  test "skill power should not be less or equal than 0" do
     @skill.power = 0
-    assert_not @skill.valid?, "skill power should be greater than 0"
+    assert_not @skill.valid?, "skill power should not be 0"
 
     @skill.power = -10
-    assert_not @skill.valid?, "skill power should be greater than 0"
+    assert_not @skill.valid?, "skill power should not be less than 0"
+  end
 
+  test "skill power should not be string" do
     @skill.power = "a"
     assert_not @skill.valid?, "skill power should not be string"
   end
 
-  test "power is valid" do
+  test "skill power should be valid" do
     @skill.power = "500"
     assert @skill.valid?, "skill power should be valid"
   end
 
-  test "max pp is not valid" do
+  test "skill max pp should not be less or equal than 0" do
     @skill.max_pp = 0
-    assert_not @skill.valid?, "skill max_pp should be greater than 0"
+    assert_not @skill.valid?, "skill max_pp should not be 0"
 
     @skill.max_pp = -10
-    assert_not @skill.valid?, "skill max_pp should be greater than 0"
+    assert_not @skill.valid?, "skill max_pp should not be less than 0"
+  end
 
+  test "skill max pp should not be string" do
     @skill.max_pp = "a"
     assert_not @skill.valid?, "skill max_pp should not be string"
   end
 
-  test "max pp is valid" do
+  test "skill max pp should be valid" do
     @skill.max_pp = "50"
     assert @skill.valid?, "skill max_pp should be valid"
   end
-
 end
